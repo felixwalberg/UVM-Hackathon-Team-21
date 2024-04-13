eaddd4ebf4fcc2ebe3e3d489510c809da7a93275
Rustum Zia <ziarustum@gmail.com>
2024-04-13 15:33:08 -0400

    map.js] pull arrival data

--- static/map.js
+++ static/map.js
@@ -1,7 +1,7 @@
 bus_markers = []
 bus_routes = []
 
-async function updatePositions() {
+async function buses() {
     const response = await fetch("/data/vehicles", {
         method: "POST",
     });
@@ -10,18 +10,56 @@ async function updatePositions() {
     for (const marker of bus_markers) {
         marker.remove()
     }
-    
+
     for (const bus of data['gmt']) {
         bus_markers.push(L.marker([bus.lat, bus.lon], {icon: bus1}).addTo(map))
     }
     for (const bus of data['cat']) {
         bus_markers.push(L.marker([bus.lat, bus.lon], {icon: bus1}).addTo(map))
     }
-
-    // setBusPosition(1, data[0].lat, data[0].lon);
 }
 
-updatePositions()
+async function arrivals() {
+    const response = await fetch("/data/arrivals", {
+        method: "POST",
+    });
+    const data = await response.json();
+
+    table = `
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th>Bus</th>
+                    <th>Arrival</th>
+                </tr>
+            </thead>
+            <tbody id="temp_tbody">
+            `
+
+    for (const stop of data) {
+        for (const arrival of stop.data) {
+            table += `
+            <tr>
+                <td>${stop.name}</td>
+                <td>${arrival.formattedTime}</td>
+            </tr>
+            `
+        }
+    }
+
+    table += `
+            </tbody>
+        </table>
+    `
+}
+
+
+async function pullData() {
+    await buses();
+    await arrivals();
+}
+
+pullData()
 setInterval(() => {
-    updatePositions();
+    pullData();
 }, 5000)
