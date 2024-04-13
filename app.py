afe60b42b8fb2d5833df039fd900b4fc2992c95d
Rustum Zia <ziarustum@gmail.com>
2024-04-13 15:32:51 -0400

    [app.py] get arrival data

--- app.py
+++ app.py
@@ -6,8 +6,13 @@ app = Flask(__name__)
 
 GMT_VEHICLES_URL = "https://maps.trilliumtransit.com/gtfsmap-realtime/feed/ccta-vt-us/vehicles"
 CAT_VEHICLES_URL = "https://feeds.transloc.com/3/vehicle_statuses?agencies=603&include_arrivals=true"
+# Arrivals must be queried with individual stopcodes
+GMT_ARRIVALS_URL = "https://maps.trilliumtransit.com/gtfsmap-realtime/feed/ccta-vt-us/arrivals?stopCode="
 GMT_ROUTES_URL = "https://gtfs-api.trilliumtransit.com/gtfs-api/routes/by-feed/ccta-vt-us"
 GMT_STOPS_URL = "https://gtfs-api.trilliumtransit.com/gtfs-api/stops/by-feed/ccta-vt-us/route-id/19137,19139,13496,13497,13498,3190,3191,7458,11182,7455,74419,74409,74410,74411,19141,74418,74413,3175,74412,3153,3176,3167,3177,3181,3168,3183,3197,3194,19140,3195,3196,3186,7335,74414,74415,3171,3172,3174,3188,74416,74417,3163,3164,3165,19138,32941,32942,11183,19143,19142,19144,19145,"
+
+DAVIS_CENTER_GMT_STOPCODE = "805489"
+
 
 @app.route("/", methods=["GET"])
 def home():
@@ -16,7 +21,13 @@ def home():
 @app.route("/data/vehicles", methods=["POST"])
 def vehicleData():
     # TODO: handle failure to fetch data
-    return jsonify(get_vehicles());
+    return jsonify(vehicles());
+
+@app.route("/data/arrivals", methods=["POST"])
+def arrivalsData():
+    return jsonify([
+        gmt_arrivals("Davis/main st", DAVIS_CENTER_GMT_STOPCODE)
+    ]);
 
 def gmt_buses():
     res = requests.get(GMT_VEHICLES_URL)
@@ -44,12 +55,24 @@ def cat_buses():
              'lat': bus['position'][0],
              'lon': bus['position'][1]} for bus in data['vehicles']]
 
-
-def get_vehicles():
+def vehicles():
     return {
         'gmt': gmt_buses(),
         'cat': cat_buses()
     }
+
+
+def gmt_arrivals(name, stopcode):
+    res = requests.get(GMT_ARRIVALS_URL + stopcode)
+    data = res.json()
+    if data['status'] != 'success':
+        return None
+    return {
+        'name': name,
+        'type': 'gmt',
+        'data': data['data']
+    }
+
 
 if __name__=="__main__":
     app.run(debug=True)
