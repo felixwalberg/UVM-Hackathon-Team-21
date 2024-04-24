dc93dadd349a0e5b07f16025be9235e130c77b01
Rustum Zia <ziarustum@gmail.com>
2024-04-23 22:38:38 -0400

    fix paths

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, jsonify
 import requests
 import json
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='public', static_url_path='')
 
 GMT_VEHICLES_URL = "https://maps.trilliumtransit.com/gtfsmap-realtime/feed/ccta-vt-us/vehicles"
 CAT_VEHICLES_URL = "https://feeds.transloc.com/3/vehicle_statuses?agencies=603&include_arrivals=true"
@@ -10,12 +10,11 @@ CAT_VEHICLES_URL = "https://feeds.transloc.com/3/vehicle_statuses?agencies=603&i
 GMT_ARRIVALS_URL = "https://maps.trilliumtransit.com/gtfsmap-realtime/feed/ccta-vt-us/arrivals?stopCode="
 GMT_ROUTES_URL = "https://gtfs-api.trilliumtransit.com/gtfs-api/routes/by-feed/ccta-vt-us"
 GMT_STOPS_URL = "https://gtfs-api.trilliumtransit.com/gtfs-api/stops/by-feed/ccta-vt-us/route-id/19137,19139,13496,13497,13498,3190,3191,7458,11182,7455,74419,74409,74410,74411,19141,74418,74413,3175,74412,3153,3176,3167,3177,3181,3168,3183,3197,3194,19140,3195,3196,3186,7335,74414,74415,3171,3172,3174,3188,74416,74417,3163,3164,3165,19138,32941,32942,11183,19143,19142,19144,19145,"
-STOP_DATA = "static/routes.json"
+STOP_DATA = "public/routes.json"
 
 DAVIS_CENTER_GMT_STOPCODE = "805489"
 UHEIGHT_STOPCODE = "805490"
 MED_STOPCODE = "805757"
-
 
 @app.route("/", methods=["GET"])
 def home():
